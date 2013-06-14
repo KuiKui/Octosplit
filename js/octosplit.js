78525fab1e6bf21d12cf63ab59615802632bc8f9
Evelio Tarazona Cáceres <evelio@evelio.info>
2013-06-13 22:12:56 -0700

    Added whitespace checkbox to add/remove w=1 to URL

--- js/octosplit.js
+++ js/octosplit.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   addWordWrapChekbox();
   addSideBySideCheckbox();
+  addWhitespaceCheckbox();
   manageNewComment();
   manageTabs();
 });
@@ -28,6 +29,17 @@ function addSideBySideCheckbox() {
     }
   };
   addOneCheckbox('octosplit', 'octicon-mirror-public', 'Side by side', $clickFn, false);
+}
+
+function addWhitespaceCheckbox() {
+  var $clickFn = function(event) {
+    if ($(this).is(':checked')) {
+      removeWhitespaceParam();
+    } else {
+      addWhitespaceParam();
+    }
+  };
+  addOneCheckbox('whitespace', 'octicon-telescope', 'Whitespace', $clickFn, !hasWhitespaceParam()); // hasWhitespaceParam tell us that we should ignore whitespace, so checkmark is inverse
 }
 
 function addOneCheckbox($id, $labelSpanClasses, $labelInner, $clickFn, $checked) {
@@ -185,4 +197,55 @@ function isSplittable($table) {
 
 function isResettable($table) {
   return ($('.new-number', $table).length > 0)
+}
+
+
+function hasWhitespaceParam() {
+  return getURLParameter('w') !== null; // Right now w can have any value as long is present gh will remove whitespace
+}
+
+function removeWhitespaceParam() {
+  document.location = removeParameter(document.location.search, 'w');
+}
+
+function addWhitespaceParam() {
+  if (!hasWhitespaceParam()) {
+    insertParameter('w', '1');
+  }
+}
+
+// Some Utility methods
+// Barely based on http://stackoverflow.com/questions/486896/adding-a-parameter-to-the-url-with-javascript
+function insertParameter(key, value) {
+  key = encodeURIComponent(key); value = encodeURIComponent(value);
+
+  var s = removeParameter(document.location.search, key); // We remove it in case is there already
+  var kvp = key+"="+value;
+
+  s += (s.indexOf('?') !== -1 ? '&' : '?') + kvp;
+
+  document.location.search = s;
+}
+// Stolen from http://stackoverflow.com/questions/1634748/how-can-i-delete-a-query-string-parameter-in-javascript
+function removeParameter(url, parameter) {
+  var urlparts= url.split('?');
+
+  if (urlparts.length>=2) {
+    var urlBase=urlparts.shift(); //get first part, and remove from array
+    var queryString=urlparts.join("?"); //join it back up
+
+    var prefix = encodeURIComponent(parameter)+'=';
+    var pars = queryString.split(/[&;]/g);
+    if (pars.length > 0) {
+      for (var i= pars.length; i-->0;)               //reverse iteration as may be destructive
+        if (pars[i].lastIndexOf(prefix, 0)!==-1)   //idiom for string.startsWith
+          pars.splice(i, 1);
+      url = urlBase+'?'+pars.join('&');
+    }
+  }
+  return url;
+}
+// From http://stackoverflow.com/questions/1403888/get-url-parameter-with-jquery
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(document.location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
