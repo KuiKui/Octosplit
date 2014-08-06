ef6c561660be55382c945fd2de6cb8c8de2152e5
Stefano Pogliani <author@spogliani.net>
2014-08-06 21:11:05 +0100

    Updated css classes to match new GitHub names.

--- data/js/octosplit.js
+++ data/js/octosplit.js
@@ -148,17 +148,21 @@ function splitDiffLine($line) {
   var $oldLOC = $('<td class="diff-line-code"></td>');
   var $newLOC = $('<td class="diff-line-code"></td>');
 
-  if ($line.hasClass('gd')) {
+  if ($oldNumber.hasClass('diff-line-num-deletion')) {
     $oldLOC.html($LOC.html());
+    $oldLOC.attr('class', $LOC.attr('class'));
     $newLOC.addClass('nd');
     $newNumber.addClass('nd');
     $newLOC.html('');
-  } else if ($line.hasClass('gi')) {
+  } else if ($newNumber.hasClass('diff-line-num-addition')) {
     $oldLOC.html('');
+    $newLOC.attr('class', $LOC.attr('class'));
     $newLOC.html($LOC.html());
     $oldLOC.addClass('nd');
     $oldNumber.addClass('nd');
   } else {
+    // NOTE: I'm unsure of what this was ment for.
+    //       Class gc no longer exists and nothing seems broken.
     if ($line.hasClass('gc')) {
       $oldLOC.addClass('gc');
       $newLOC.addClass('gc');
@@ -186,14 +190,14 @@ function resetDiffLine($line) {
   var $newNumber = $($children[2]);
   var $newLOC    = $($children[3]);
 
-  if($line.hasClass('gd')) {
+  if ($oldNumber.hasClass('diff-line-num-deletion')) {
     $newLOC.html($oldLOC.html());
+    $newLOC.attr('class', $oldLOC.attr('class'));
   }
 
   $oldLOC.remove();
 
   $oldNumber.removeClass('nd');
-  $oldNumber.css('border-right', 'none');
   $newNumber.removeClass('nd');
   $newLOC.removeClass('nd');
 }
@@ -213,7 +217,7 @@ function isFilesBucketTab() {
 }
 
 function isSplittable($table) {
-  return ($('tr.gd', $table).length || $('tr.gi', $table).length);
+  return ($('tr > td.diff-line-num-deletion', $table).length || $('tr > td.diff-line-num-addition', $table).length);
 }
 
 function isResettable($table) {
